967dbf09667c42ab6de07628c531b84a9950cde1
Diana-Victoria Tiriplica <diana.tiriplica@gmail.com>
2012-03-16 17:04:05 +0200

    Test for myspace scavenger.

---
+++ myspace_test.py
@@ -0,0 +1,40 @@
+# Copyright 2012 Sunnytrail Insight Labs Inc. All rights reserved.
+# Author: diana.tiriplica@gmail.com (Diana-Victoria Tiriplica)
+#         tabara.mihai@gmail.com (Mihai Tabara)
+
+import unittest
+import thread
+import beanstalkc
+import simplejson
+from scavengers.scavenger import Scavenger
+from scavengers.scavenger_utils import OK_CODE
+from scavengers.myspace_scavenger import MySpaceScavenger
+
+NAME = "myspace"
+IN = NAME + "_in"
+OUT = NAME + "_out"
+EMAIL = "dia_tiriplica@yahoo.co.uk"
+TIMEOUT = 10
+
+class MySpaceTest(unittest.TestCase):
+  def setUp(self):
+    self.scavenger = MySpaceScavenger(NAME, IN, OUT)
+    thread.start_new_thread(self.scavenger.run, ())
+
+    beanstalk = beanstalkc.Connection()
+    beanstalk.use(IN)
+    beanstalk.put(EMAIL)
+
+    beanstalk.watch(OUT)
+    job = beanstalk.reserve(timeout=TIMEOUT)
+    self.response = simplejson.loads(job.body)
+    job.delete()
+
+  def test_status(self):
+    self.assertEqual(self.response['status'], OK_CODE)
+
+  def test_email(self):
+    self.assertEqual(self.response['email'], EMAIL)
+
+if __name__=="__main__":
+  unittest.main()
